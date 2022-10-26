2e7ea4c98eeadcd30836f71a1d5e9961f773ebd7
omarbabou <obabou50@gmail.com>
2022-10-26 13:26:11 +0200

    Add music album publish_date to the project

--- graphics/add_music_album.rb
+++ graphics/add_music_album.rb
@@ -18,3 +18,18 @@ def music_album_data
   spotify = true if on_spotify == 'y'
   [publish_date, spotify]
 end
+
+def convert_to_js(album)
+    File.write('album.json', '[]') unless File.exist?('album.json')
+    albums = JSON.parse(File.read('album.json'))
+    albums << { 'id' => album.id,
+                'publish_date' => album.publish_date, 'on_spotify' => album.on_spotify, 'genre' => album.genre.name }
+    File.write('album.json', JSON.generate(albums))
+  end
+  
+  def include_genre(album)
+    puts 'Name of genre: '
+    genre_name = gets.chomp
+    genre = Genre.new(genre_name)
+    album.add_genre(genre)
+  end
