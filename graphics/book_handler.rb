52d8049c82efa1ab552378a09030936a996d8502
omarbabou <obabou50@gmail.com>
2022-10-26 13:27:05 +0200

    Add Book handler data to the project

---
+++ graphics/book_handler.rb
@@ -0,0 +1,23 @@
+require_relative '../classes/book'
+
+class BookHandler
+  def initialize
+    @file_data = nil
+    @new_book = nil
+    @destructured_array = nil
+  end
+
+  def load_file
+    file_to_load = File.read('books.json')
+    JSON.parse(file_to_load)
+  end
+
+  def load_file_if_it_exist
+    load_file unless File.file?('books.json') == false
+  end
+
+  def create_file
+    new_file = File.new('books.json', 'w')
+    new_file.puts(JSON.pretty_generate([]))
+    new_file.close
+  end
