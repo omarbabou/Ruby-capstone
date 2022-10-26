8f3576036d7b935b6703eb6cf20fd3bb5aa58d58
omarbabou <obabou50@gmail.com>
2022-10-26 13:52:56 +0200

    Create AuthorsDataController module

---
+++ modules/author_module.rb
@@ -0,0 +1,14 @@
+require_relative '../apps/author'
+require 'json'
+
+module AuthorsDataController
+  def load_authors
+    file = 'authors.json'
+    data = []
+    if File.exist?(file) && File.read(file) != ''
+      JSON.parse(File.read(file)).each do |element|
+        data.push(Author.new(element['first_name'], element['last_name']))
+      end
+    end
+    data
+  end
