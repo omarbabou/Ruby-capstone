db9b2ed260ae80e7c7c4e0ffae1f769330981de8
omarbabou <obabou50@gmail.com>
2022-10-26 13:28:36 +0200

    Add author, publisher, label and publish date data

--- graphics/book_handler.rb
+++ graphics/book_handler.rb
@@ -1,4 +1,4 @@
-require_relative '../classes/book'
+require_relative '../apps/book'
 
 class BookHandler
   def initialize
@@ -20,4 +20,28 @@ class BookHandler
     new_file = File.new('books.json', 'w')
     new_file.puts(JSON.pretty_generate([]))
     new_file.close
+  end
+
+  def read_file_if_it_exists
+    create_file unless File.file?('books.json')
+    @file_data = File.read('books.json')
+  end
+
+  def append_to_array
+    @destructured_array = JSON.parse(@file_data)
+
+    @destructured_array << { 'author' => @new_book.author,
+                             'publisher' => @new_book.publisher,
+                             'cover_state' => @new_book.cover_state,
+                             'label' => @new_book.label,
+                             'publish_date' => @new_book.publish_date }
+  end
+
+  def preserve_data
+    read_file_if_it_exists
+    append_to_array
+
+    File.open('books.json', 'w') do |f|
+      f.puts JSON.pretty_generate(@destructured_array)
+    end
   end
