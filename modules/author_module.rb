43eea87a05a768aab17a2235bb1b18986a76f288
omarbabou <obabou50@gmail.com>
2022-10-26 13:54:02 +0200

    Add save_authors function

--- modules/author_module.rb
+++ modules/author_module.rb
@@ -12,3 +12,12 @@ module AuthorsDataController
     end
     data
   end
+
+  def save_authors
+    data = []
+    @authors.each do |author|
+      data.push({ first_name: author.first_name, last_name: author.last_name })
+    end
+    File.write('authors.json', JSON.generate(data))
+  end
+end
